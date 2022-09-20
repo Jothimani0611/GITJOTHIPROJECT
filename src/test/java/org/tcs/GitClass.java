ce79f1a85b33534ba4765a52de4e1b106bb2666d
Jothimani0611 <jothi611mani@gmail.com>
2022-09-20 22:49:15 +0530

    I am jothi

--- src/test/java/org/tcs/GitClass.java
+++ src/test/java/org/tcs/GitClass.java
@@ -1,5 +1,15 @@
 package org.tcs;
 
 public class GitClass {
+	private void jothi1() {
+		System.out.println("I am fun1");
+
+	}
+	private void jothi2() {
+		System.out.println("Ia m fun2");
+	}
+	private void jothi3() {
+		System.out.println("I am fun3");
+	}
 
 }
